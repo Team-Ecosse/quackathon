b87d6a53eee51c9c9a1a19e77ccd7b8eb4c0361e
link5478 <cheyne.carsten@gmail.com>
2017-11-11 21:09:50 +0000

    Added basic Collectable and Obstacle Objects

---
+++ Assets/Scripts/CollectableController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableController : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Collected Collectable");
+            //PlayerScore playerScore = collision.gameObject.GetComponent<PlayerScore>();
+            //playerScore.AddPoints();
+        }
+
+    }
+}
---
+++ Assets/Scripts/ObstacleController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleController : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Hit Player");
+
+            //PlayerHealth playerHealth = collision.gameObject.GetComponent<PlayerHealth>();
+            //playerHealth.TakeDamage();
+
+        }
+    }
+}

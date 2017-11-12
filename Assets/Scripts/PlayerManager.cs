68515f30a134c83d19fc2bbe30c8382a21c9dbaa
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-11-12 00:19:01 +0000

    Prepare merge

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour {
+
+
+    int playerHealth;
+
+    // Use this for initialization
+    void Start () {
+
+        playerHealth = 3;
+		
+	}
+	
+
+    public void playerTakenDMG(int damage){
+
+        playerHealth -= damage;
+
+    }
+
+    public void playerGainHealth(int heal)
+    {
+
+        playerHealth += heal;
+
+    }
+
+    public int returnPlayerHealth()
+    {
+
+        return playerHealth;
+    }
+}
---
+++ Assets/Scripts/PlayerScoreUI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScoreUI : MonoBehaviour
+{
+
+    Text text;
+
+    public GameManager GameManager;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text = this.GetComponent<Text>();
+        text.text = GameManager.ReturnPlayerSco().ToString();
+    }
+}

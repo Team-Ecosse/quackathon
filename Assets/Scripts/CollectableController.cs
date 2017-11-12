90ec36d676b4d21e8e0dc017dba30d03f9df12b6
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-11-12 00:01:57 +0000

    Prepare commit

---
+++ Assets/Scripts/CollectableController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableController : MonoBehaviour
+{
+
+    private void Awake(){
+
+        int score = 10;
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.tag == "Player")
+        {
+
+            Debug.Log("Score added");
+            //PlayerScore playerScore = collision.gameObject.GetComponent<PlayerScore>();
+            //playerScore.AddScore(score);
+        }
+    }
+}
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.Rendering;
+
+public class MainMenu : MonoBehaviour {
+
+    [Header("Required Object Refs.")]
+    [Space]
+
+    private GameObject _eventSystem;
+    private GameManager _gameManager;
+
+    [Space]
+    [Header("UI Container Objects")]
+    [Space]
+
+    public GameObject _mainMenuUI;
+
+    void Start () {
+
+        _eventSystem = GameObject.Find("EventSystem");
+        _gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+
+        _mainMenuUI.SetActive(true);
+	}
+
+    public void StartGame() {
+        _gameManager.ProgressToNextScene();
+    }
+
+    public void ViewHighScores() {
+
+        if(_mainMenuUI.activeInHierarchy) {
+            _mainMenuUI.SetActive(false);
+
+            _eventSystem.GetComponent<UnityEngine.EventSystems.EventSystem>()
+                .SetSelectedGameObject(GameObject.Find("ReturnToMainMenu"));
+        }
+    }
+
+    public void ExitGame() {
+        _gameManager.ExitGame();
+    }
+
+    public void ReturnToMainMenu() {
+        
+        _mainMenuUI.SetActive(true);
+
+        _eventSystem.GetComponent<UnityEngine.EventSystems.EventSystem>()
+                .SetSelectedGameObject(GameObject.Find("StartGame"));
+    }
+}
